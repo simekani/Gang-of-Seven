186afb408815d6dcb9dad5c2a20e3ed60bdb3f34
Simekani <simekani.mabambe@gmail.com>
2020-11-07 18:51:37 +0200

    Draft 1

---
+++ Testing/TestingTeam.cpp
@@ -0,0 +1,26 @@
+#include "TestingTeam.h"
+
+void TestingTeam::sendMessage(bool result) {
+	// TODO - implement TestingTeam::sendMessage
+	
+}
+
+void TestingTeam::receiveMessage(Part* part) {
+	// TODO - implement TestingTeam::receiveMessage
+	
+}
+
+void TestingTeam::recieveMessage(Car* model) {
+	// TODO - implement TestingTeam::recieveMessage
+	
+}
+
+void TestingTeam::testCarModel(Car* model) {
+	// TODO - implement TestingTeam::testCarModel
+	
+}
+
+void TestingTeam::simulatePartPerformance(Part* part) {
+	// TODO - implement TestingTeam::simulatePartPerformance
+	
+}
