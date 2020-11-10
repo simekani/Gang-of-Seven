3e4c9f6f5446589e18217bc609669a90b37205f8
Simekani <simekani.mabambe@gmail.com>
2020-11-10 08:37:03 +0200

    Modified racing strategy

--- RaceStrategy/Aggressive.cpp
+++ RaceStrategy/Aggressive.cpp
@@ -14,15 +14,19 @@ Aggressive::Aggressive(string StrategyName) {
 
 void Aggressive::strategyBlueprint() {
 	// TODO - implement Aggressive::strategyBlueprint
-	cout<<endl<< "The driver will be using Aggressive Race Strategy!"<<endl; 
+
+	cout<<endl<<"------------------------------------------------------------"<<endl; 
+	cout<< "The driver will be using Aggressive Race Strategy!"<<endl; 
 	cout<<Name<<" Aggressive Strategy Blueprint"<<endl; 
 
 	cout<< "This Implies that the "<<numberOfPitstops<<" pitstop strategy will be taken"<<endl;
 	cout<< "Start with soft tires"<<endl;
 	cout<< "Pitstop 1(1/3 of race): Tyres replaced with soft tires"<<endl;
-	cout<< "Pitstop 2(2/3 of race): Tyres replaced with Medium tires"<<endl<<endl;
+	cout<< "Pitstop 2(2/3 of race): Tyres replaced with Medium tires"<<endl;
 
 
 	cout<< "The Aggressive Strategy is highly succesfull when there is a safe car"<<endl; 
-	cout<< "The Aggressive Strategy could fail if pitstops take too long"<<endl<<endl; 
+	cout<< "The Aggressive Strategy could fail if pitstops take too long"<<endl; 
+	cout<<"------------------------------------------------------------"<<endl<<endl; 
+
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 
 main:
-	g++ -g -std=c++0x  Racing/*.h Racing/*.cpp RaceStrategy/*.cpp RaceStrategy/*.h Logistics/*.cpp Logistics/*.h Testing/*.cpp Testing/*.h Simulator/*.cpp Simulator/*.h Engineering/*.cpp  Engineering/*.h  Mediator/*.cpp Mediator/*.h *.cpp -o main
+	g++ -g -std=c++0x  Racing/*.h Racing/*.cpp RaceStrategy/*.cpp RaceStrategy/*.h Testing/*.cpp Testing/*.h Simulator/*.cpp Simulator/*.h Engineering/*.cpp  Engineering/*.h  Mediator/*.cpp Mediator/*.h *.cpp -o main
 run:	main
 	./main
 
